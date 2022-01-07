32db80adf6d268c898e853fd52b6a874e8d3aa8a
SK SINGH <singhsushil5277@gmail.com>
2022-01-07 22:35:42 +0545

    Added remaining component using shortcuts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent,
+    PostComponent,
+    PostEditComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
