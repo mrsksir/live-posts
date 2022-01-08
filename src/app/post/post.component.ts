45660f5208e137b3741fca6b0c25f6c1ee53109f
SK SINGH <singhsushil5277@gmail.com>
2022-01-08 10:16:20 +0545

    Use of @Input() Decorator to receive the post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  // receive post object from post-list component
+  @Input() post?: Post;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.post);
   }
-
 }
