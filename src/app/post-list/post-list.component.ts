0c0a527235f6008da7e4f8f7abdf82dfc84de280
SK SINGH <singhsushil5277@gmail.com>
2022-01-08 09:54:38 +0545

    Created array of Post objects

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent implements OnInit {
+  listOfPosts: Post[] = [
+    new Post(
+      'Nature1',
+      `2Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsum 
+      obcaecatiducimus nostrum laboriosam excepturi nisi quos beatae dolorum voluptatibus enim animi, 
+      tenetur maximeut est expedita vero et cupiditate?`,
+      `https://curlytales.com/wp-content/uploads/2020/08/featurenature1-1170x614.jpg`,
+      `test@test.com`,
+      new Date()
+    ),
 
-  constructor() { }
+    new Post(
+      'Nature2',
+      `2Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsum 
+      obcaecatiducimus nostrum laboriosam excepturi nisi quos beatae dolorum voluptatibus enim animi, 
+      tenetur maximeut est expedita vero et cupiditate?`,
+      `https://curlytales.com/wp-content/uploads/2020/08/featurenature1-1170x614.jpg`,
+      `test@test.com`,
+      new Date()
+    ),
 
-  ngOnInit(): void {
-  }
+    new Post(
+      'Nature3',
+      `3Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsum 
+      obcaecatiducimus nostrum laboriosam excepturi nisi quos beatae dolorum voluptatibus enim animi, 
+      tenetur maximeut est expedita vero et cupiditate?`,
+      `https://curlytales.com/wp-content/uploads/2020/08/featurenature1-1170x614.jpg`,
+      `test@test.com`,
+      new Date()
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
